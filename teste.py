76665bda5525bb1ef4f80174f7c6283f3fb5263a
The Ape <jonasmito@gmail.com>
2024-06-20 11:18:18 +0100

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,10 @@
 print("Hello World")
 
+testing = input("Did you have a good day (y/n): ").lower()
+
+if testing == "y":
+    print("Very nice")
+elif testing == "n":
+    print("I hope better days come")
+else:
+    print("not a valid answer lol")
